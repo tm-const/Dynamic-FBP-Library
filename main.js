413372b343177c7ca9c07fc2c5c201376f7adbfc
Manny <55524897+tm-const@users.noreply.github.com>
2020-07-21 15:41:42 -0400

    Update main.js

--- main.js
+++ main.js
@@ -812,3 +812,107 @@ window.fbAsyncInit = function() {
   //   data-controls="false"></div>
 
 });
+
+
+
+// *----------------------------------------
+// Scroll Percentage
+// *----------------------------------------
+
+var executeWhenReachedPagePercentage = function(percentage, callback) {
+    if (typeof percentage !== 'number') {
+      console.error(
+        'First parameter must be a number, got',
+        typeof percentage,
+        'instead',
+      );
+    }
+
+    if (typeof callback !== 'function') {
+      console.error(
+        'Second parameter must be a function, got',
+        typeof callback,
+        'instead',
+      );
+    }
+
+    function getDocumentLength() {
+      var D = document;
+      return Math.max(
+          D.body.scrollHeight, D.documentElement.scrollHeight,
+          D.body.offsetHeight, D.documentElement.offsetHeight,
+          D.body.clientHeight, D.documentElement.clientHeight
+      )
+    }
+
+    function getWindowLength() {
+      return window.innerHeight || 
+        (document.documentElement || document.body).clientHeight;
+    }
+
+    function getScrollableLength() {
+      if (getDocumentLength() > getWindowLength()) {
+        return getDocumentLength() - getWindowLength();
+      } else {
+        return 0;
+      }
+    }
+
+    var scrollableLength = getScrollableLength();
+
+    window.addEventListener("resize", function(){
+      scrollableLength = getScrollableLength();
+    }, false)
+
+    function getCurrentScrolledLengthPosition() {
+     return window.pageYOffset || 
+       (document.documentElement || document.body.parentNode || document.body).scrollTop;
+    }
+
+    function getPercentageScrolled() {
+      if (scrollableLength == 0) {
+        return 100;
+      } else {
+        return getCurrentScrolledLengthPosition() / scrollableLength * 100;
+      }
+    }
+
+    var executeCallback = (function() {
+      var wasExecuted = false;
+      return function() {
+        if (!wasExecuted && getPercentageScrolled() > percentage) {
+          wasExecuted = true;
+          callback();
+        }
+      };
+    })();
+
+    if (getDocumentLength() == 0 ||
+      (getWindowLength()/getDocumentLength() * 100 >= percentage)) {
+      callback();
+    } else {
+      window.addEventListener('scroll', executeCallback, false);
+    }
+  };
+
+  executeWhenReachedPagePercentage(25, function() {
+    fbq('trackCustom', '25PS', {
+      content_name: 'flc, salespage',
+    });
+  });
+  executeWhenReachedPagePercentage(50, function() {
+    fbq('trackCustom', '50PS', {
+      content_name: 'flc, salespage',
+    });
+  });
+  executeWhenReachedPagePercentage(75, function() {
+    fbq('trackCustom', '75PS', {
+      content_name: 'flc, salespage',
+    });
+  });
+  executeWhenReachedPagePercentage(90, function() {
+    fbq('trackCustom', '100PS', {
+      content_name: 'flc, salespage',
+    });
+
+  });
